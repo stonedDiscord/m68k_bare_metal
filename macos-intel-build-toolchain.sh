100801934d7645fa99668405021bf1d49ef2b1cf
stonedDiscord <Tukz@gmx.de>
2026-03-18 10:33:53 +0000

    zeroes

--- macos-intel-build-toolchain.sh
+++ macos-intel-build-toolchain.sh
@@ -24,7 +24,7 @@ PREFIX=$PWD/toolchain/$TARGET
 MIRROR=http://ftpmirror.gnu.org
 # MIRROR=https://mirror.team-cymru.com/gnu
 
-BINUTILS=binutils-2.46
+BINUTILS=binutils-2.46.0
 BINUTILS_URL=$MIRROR/binutils/$BINUTILS.tar.xz
 
 GCCVER=15.2.0
