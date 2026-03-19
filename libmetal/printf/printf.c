7297537b8bcc9f0877918107a7c70dbac249ef95
stonedDiscord <Tukz@gmx.de>
2026-03-19 11:24:03 +0100

    revert putchar_

--- libmetal/printf/printf.c
+++ libmetal/printf/printf.c
@@ -238,7 +238,7 @@ static inline void out_putchar(char character, void* buffer, size_t idx, size_t 
 {
   (void)buffer; (void)idx; (void)maxlen;
   if (character) {
-    _putchar(character);
+    putchar_(character);
   }
 }
 
